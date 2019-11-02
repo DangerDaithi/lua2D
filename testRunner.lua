415064b36467f2f87927da5787d9e140337a22e7
Fekshost <pope.david12@gmail.com>
2019-11-02 15:34:41 +0000

    Player teleportation.

--- testRunner.lua
+++ testRunner.lua
@@ -20,14 +20,14 @@ worldObject_metatab.__index = WorldObject
 LevelDataPoint = {}
 local levelDataPoint_metatab = {}
 
-function LevelDataPoint.new(x, y, isPlayerAccesible, worldObjects)
+function LevelDataPoint.new(x, y, worldObjects, isPlayerAccesible)
 	local info = {}
 	info.x = x
 	info.y = y
-	if isPlayerAccesible == nil then -- bit smelly setting default value like this
+	info.worldObjects = worldObjects
+	if isPlayerAccesible == nil then -- bit smelly, but setting default value like this
 		isPlayerAccesible = true
 	end
-	info.worldObjects = worldObjects
 	info.isPlayerAccesible = isPlayerAccesible
 	setmetatable(info, levelDataPoint_metatab)
 	return info
@@ -64,6 +64,10 @@ function Level.load(levelData)
 	level_metatab.levelData = levelData
 end
 
+function Level.getLevelData()
+	return level_metatab.levelData
+end
+
 level_metatab.__index = Level
 
 
@@ -77,6 +81,9 @@ function Player.new(id, level)
 	local info = {}
 	info.id = id
 	info.level = level
+	info.maxBackpackCapacity = 5
+	info.backpack = {} -- player inventory
+	info.currentPosition = LevelDataPoint.new(0, 0, {})
 	setmetatable(info, player_metatab)
 	return info
 end
@@ -94,6 +101,34 @@ function Player:getLevel()
 	return player_metatab.level
 end
 
+function Player:teleport(x, y)
+	if player_metatab.level == nil then
+		print("Must register a level to the player before teleporting.")
+		return
+	end
+
+	if player_metatab.level.getLevelData()[x..y] == nil then
+		print("Player has attempted to stray out of the permitted world bounds. This is bold")
+		return
+	end
+
+	-- coords exist
+
+	if player_metatab.level.getLevelData()[x..y]:playerAccesible() then
+
+		if player_metatab.currentPosition == nil then -- check for null reference exception
+			print("curent position was nil")
+			player_metatab.currentPosition = LevelDataPoint.new(0, 0, {})
+		end
+		print("Player has moved from  ("..player_metatab.currentPosition:getX()..","..player_metatab.currentPosition:getY() ..") to ("..x..","..y..")")
+		player_metatab.currentPosition = player_metatab.level.getLevelData()[x..y] -- update the players current position
+	else
+		print("Player has attempted to enter a restricted area at coords "..x..","..y)
+	end
+
+
+end
+
 player_metatab.__index = Player
 
 
@@ -103,48 +138,55 @@ player_metatab.__index = Player
 
 function getLevelData()
 	local levelData = {}
-	table.insert(levelData, LevelDataPoint.new(150, 150))
-	table.insert(levelData, LevelDataPoint.new(150, 175))
-	table.insert(levelData, LevelDataPoint.new(150, 200))
-	table.insert(levelData, LevelDataPoint.new(150, 225))
-	table.insert(levelData, LevelDataPoint.new(150, 250))
-	table.insert(levelData, LevelDataPoint.new(150, 300))
-	table.insert(levelData, LevelDataPoint.new(150, 350))
-	table.insert(levelData, LevelDataPoint.new(175, 150))
-	table.insert(levelData, LevelDataPoint.new(175, 175))
-	table.insert(levelData, LevelDataPoint.new(175, 200))
-	table.insert(levelData, LevelDataPoint.new(175, 225))
-	table.insert(levelData, LevelDataPoint.new(175, 250))
-	table.insert(levelData, LevelDataPoint.new(175, 300))
-	table.insert(levelData, LevelDataPoint.new(175, 350))
-	table.insert(levelData, LevelDataPoint.new(200, 150))
-	table.insert(levelData, LevelDataPoint.new(200, 175))
-	table.insert(levelData, LevelDataPoint.new(200, 200))
-	table.insert(levelData, LevelDataPoint.new(200, 225))
-	table.insert(levelData, LevelDataPoint.new(200, 250))
-	table.insert(levelData, LevelDataPoint.new(200, 300))
-	table.insert(levelData, LevelDataPoint.new(200, 350))
-	table.insert(levelData, LevelDataPoint.new(250, 150))
-	table.insert(levelData, LevelDataPoint.new(250, 175))
-	table.insert(levelData, LevelDataPoint.new(250, 200))
-	table.insert(levelData, LevelDataPoint.new(250, 225))
-	table.insert(levelData, LevelDataPoint.new(250, 250))
-	table.insert(levelData, LevelDataPoint.new(250, 300))
-	table.insert(levelData, LevelDataPoint.new(250, 350))
-	table.insert(levelData, LevelDataPoint.new(300, 150))
-	table.insert(levelData, LevelDataPoint.new(300, 175))
-	table.insert(levelData, LevelDataPoint.new(300, 200, false)) -- player cannot go to (300, 200)
-	table.insert(levelData, LevelDataPoint.new(300, 225))
-	table.insert(levelData, LevelDataPoint.new(300, 250))
-	table.insert(levelData, LevelDataPoint.new(300, 300))
-	table.insert(levelData, LevelDataPoint.new(300, 350))
-	table.insert(levelData, LevelDataPoint.new(350, 150))
-	table.insert(levelData, LevelDataPoint.new(350, 175))
-	table.insert(levelData, LevelDataPoint.new(350, 200))
-	table.insert(levelData, LevelDataPoint.new(350, 225))
-	table.insert(levelData, LevelDataPoint.new(350, 250))
-	table.insert(levelData, LevelDataPoint.new(350, 300))
-	table.insert(levelData, LevelDataPoint.new(350, 350))
+
+	-- concatenate the x and y coords and use for key
+	levelData["150150"] = LevelDataPoint.new(150, 150, {})
+	levelData["150175"] = LevelDataPoint.new(150, 175, {})
+	levelData["150200"] = LevelDataPoint.new(150, 200, {})
+	levelData["150225"] = LevelDataPoint.new(150, 225, {})
+	levelData["150250"] = LevelDataPoint.new(150, 250, {})
+	levelData["150300"] = LevelDataPoint.new(150, 300, {})
+	levelData["150350"] = LevelDataPoint.new(150, 350, {})
+
+	levelData["175150"] = LevelDataPoint.new(175, 150, {})
+	levelData["175175"] = LevelDataPoint.new(175, 175, {})
+	levelData["175200"] = LevelDataPoint.new(175, 200, {})
+	levelData["175225"] = LevelDataPoint.new(175, 225, {})
+	levelData["175250"] = LevelDataPoint.new(175, 250, {})
+	levelData["175300"] = LevelDataPoint.new(175, 300, {})
+	levelData["175350"] = LevelDataPoint.new(175, 350, {})
+
+	levelData["200150"] = LevelDataPoint.new(200, 150, {})
+	levelData["200175"] = LevelDataPoint.new(200, 175, {})
+	levelData["200200"] = LevelDataPoint.new(200, 200, {})
+	levelData["200225"] = LevelDataPoint.new(200, 225, {})
+	levelData["200250"] = LevelDataPoint.new(200, 250, {})
+	levelData["200300"] = LevelDataPoint.new(200, 300, {})
+	levelData["200350"] = LevelDataPoint.new(200, 350, {})
+
+	levelData["250150"] = LevelDataPoint.new(250, 150, {})
+	levelData["250175"] = LevelDataPoint.new(250, 175, {})
+	levelData["250200"] = LevelDataPoint.new(250, 200, {})
+	levelData["250225"] = LevelDataPoint.new(250, 225, {})
+	levelData["250250"] = LevelDataPoint.new(250, 250, {})
+	levelData["250300"] = LevelDataPoint.new(250, 300, {})
+	levelData["250350"] = LevelDataPoint.new(250, 350, {})
+
+	levelData["300150"] = LevelDataPoint.new(300, 150, {})
+	levelData["300175"] = LevelDataPoint.new(300, 175, {})
+	levelData["300200"] = LevelDataPoint.new(300, 200, {}, false) -- player cannot go to (300, 200)
+	levelData["300225"] = LevelDataPoint.new(300, 225, {})
+	levelData["300250"] = LevelDataPoint.new(300, 250, {})
+	levelData["300300"] = LevelDataPoint.new(300, 300, {})
+	levelData["300350"] = LevelDataPoint.new(300, 350, {})
+
+	levelData["350150"] = LevelDataPoint.new(350, 150, {})
+	levelData["350175"] = LevelDataPoint.new(350, 175, {})
+	levelData["350200"] = LevelDataPoint.new(350, 200, {})
+	levelData["350225"] = LevelDataPoint.new(350, 225, {})
+	levelData["350250"] = LevelDataPoint.new(350, 250, {})
+	levelData["350300"] = LevelDataPoint.new(350, 300, {})
+	levelData["350350"] = LevelDataPoint.new(350, 350, {})
 	return levelData
 end
 
@@ -155,7 +197,19 @@ end
 
 level = Level.new()
 level.load(getLevelData()) -- lazily load level data
--- print(table.getn(getLevelData()))
+player1 = Player.new(1)
+player1:regsiterLevel(level)
+player1:teleport(200, 200) -- accessible
+player1:teleport(200, 150) -- accessible
+player1:teleport(300, 200) -- not accessible
+
+
+--test = getLevelData()["400250"]:playerAccesible()
+
+
+
+
+
 --[[
 for _,datapoint in ipairs(getLevelData()) do
         print(datapoint:playerAccesible())
@@ -163,11 +217,6 @@ for _,datapoint in ipairs(getLevelData()) do
 end
 
 ]]
-
-player1 = Player.new(1)
-player1:regsiterLevel(level)
-
-
 
 
 
